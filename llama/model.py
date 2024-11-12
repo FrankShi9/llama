3bf41268f13b05c30ff853c65667e6f198d9df75
FrankShi9 <53264047+FrankShi9@users.noreply.github.com>
2024-11-12 15:11:14 +0000

    Update model.py
    attention mask relu

--- llama/model.py
+++ llama/model.py
@@ -297,7 +297,7 @@ class Attention(nn.Module):
         values = values.transpose(1, 2) # (bs, n_local_heads, cache_len + seqlen, head_dim)
         scores = torch.matmul(xq, keys.transpose(2, 3)) / math.sqrt(self.head_dim)
         if mask is not None:
-            scores = scores + mask  # (bs, n_local_heads, seqlen, cache_len + seqlen)
+            scores = scores + F.relu(mask)  # (bs, n_local_heads, seqlen, cache_len + seqlen)
         scores = F.softmax(scores.float(), dim=-1).type_as(xq)
         output = torch.matmul(scores, values)  # (bs, n_local_heads, seqlen, head_dim)
         output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
